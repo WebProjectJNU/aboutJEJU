651dec99f4f4c3eed556189ece8d1b46bce7b1fc
hye219 <jhy_0219@daum.net>
2021-12-09 11:21:37 +0900

    php 파일 삭제

--- create_process.php
+++
@@ -1,6 +0,0 @@
-<?php
-    echo "<p>title : ".$_POST['title']."</p>";
-    echo "<p>description : ".$_POST['description']."</p>";
-    //file_put_contents('data/'.$_POST['title'], $_POST['description']);
-    //header{'Location: /aboutJEJU/index.php'};
-?>
